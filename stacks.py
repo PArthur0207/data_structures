fba19ec9c5509373e9c51c4ccf96cbb846b528ff
PArthur0207 <marayanpaularthur1114@gmail.com>
2025-10-24 23:38:39 +0800

    fixed the stacks application of linked list

--- stacks.py
+++ stacks.py
@@ -2,15 +2,43 @@ from linked_list import LinkedList
 
 ll = LinkedList()
 
-operators = ["-", "+", "=", "/", "^", "*", "(", ")"]
-
-operation = "(A + B) * C"
+operators_used = ["-", "+", "=", "/", "^", "*", "(", ")"]
+values = {
+"^": 3,
+"*": 2,
+"/": 2,
+"+": 1,
+"-": 1,
+"(": 0,
+")": 0
+}
+original_operation = "(A + B) * C"
 output = ""
 
-for item in operation:
-    if item in operators:
-        ll.insert_at_end(item)
-    else:
-        output = output + item
+operation = original_operation.replace(" ", "")
 
-print(output + str(ll.use_list()))
+for item in operation:
+    if item in operators_used:
+        if item == "(":
+            ll.insert_at_beginning(item)
+
+        elif item == ")":
+            while ll.head and ll.head.data != "(":
+                output += ll.remove_at_beginning()
+
+            if ll.head and ll.head.data == "(":
+                ll.remove_at_beginning()
+            
+        else:
+            while ll.head and ll.head.data != "(" and values[item] <= values[ll.head.data]:
+                output += ll.remove_at_beginning()
+            
+            ll.insert_at_beginning(item)
+        
+    else:
+        output += item
+
+while ll.head:
+    output += ll.remove_at_beginning()
+
+print(f"Infix: {original_operation} \nPostfix: {output}")
