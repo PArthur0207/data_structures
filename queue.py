9d868ec48a6365b187cdb484ec54696ee2eb88b0
PArthur0207 <marayanpaularthur1114@gmail.com>
2025-11-01 21:29:12 +0800

    Added dequeue_at_tail and enqueue_at_tail, and size functions in the queue file

--- queue.py
+++ queue.py
@@ -22,6 +22,39 @@ class Queue:
             return ret_node
         return None
     
+    def dequeue_at_tail(self):
+        if self.head:
+            if self.head == self.tail:
+                ret_node = self.tail
+                self.head = None
+                self.tail = None
+                return ret_node
+            current = self.head
+            while current.next != self.tail:
+                current = current.next 
+            ret_node = self.tail
+            self.tail = current
+            self.tail.next = None
+            return ret_node
+        return None
+
+    def enqueue_at_head(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+
+    def size(self):
+        count = 0
+        current = self.head
+        while current:
+            current = current.next
+            count += 1
+        return count
+
     def is_empty(self):
         return self.head == None
         
